a8460e42b6352cacd494c5b336e6337cb645827f
Jared Chamberlain <jchamberlain909@gmail.com>
2018-04-23 18:00:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let count = 0;
-  console.log("Started")
   document.body.addEventListener("keydown", (e) => {
     if(e.location===code[count]){
       count++;
